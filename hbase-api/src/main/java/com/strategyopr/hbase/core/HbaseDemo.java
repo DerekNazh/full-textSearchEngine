e65c6c5297d49bd25f8e9ad3add6bf3c02a88f34
nazh <2637600062@qq.com>
2024-03-29 21:42:37 +0800

    fix

--- hbase-api/src/main/java/com/strategyopr/hbase/core/HbaseDemo.java
+++ hbase-api/src/main/java/com/strategyopr/hbase/core/HbaseDemo.java
@@ -20,8 +20,9 @@ public class HbaseDemo {
         //1.通过用户输入关键词获取Es的bookID
 
     }
-    public static String pullJsonFromHbase() throws IOException {
+    public static String[] pullJsonFromHbase() throws IOException {
         HashMap infos = null;
+        List jsonList =null ;
         RestHighLevelClient esConn = EsConnector.getConnection();
         List tuples = new ArrayList<StringTuple2>();
         StringTuple2 hostnamePro = new StringTuple2("hbase.zookeeper.quorum", "node1,node2,node3");
@@ -38,13 +39,15 @@ public class HbaseDemo {
             HbaseDMLUtil.createTable(hbaseConn,"search_engine","book");
         }
         String[] ids = EsDemo.getIdsFromEs();
+        jsonList = new ArrayList<String>();
         for (String id : ids) {
            infos = HbaseDDLUtil.getByRowKey(hbaseConn, "search_engine", "book", id);
-
+            ObjectMapper oM = new ObjectMapper();
+            String json = oM.writeValueAsString(infos);
+            jsonList.add(json);
         }
         //2.把获取的对象元素HashMap转化为Json串给前端
-        ObjectMapper oM = new ObjectMapper();
-        String json = oM.writeValueAsString(infos);
-        return json;
+
+        return (String[]) jsonList.toArray();
     }
 }
---
+++ hbase-api/src/test/java/aa.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class aa {
+    public static void main(String[] args) {
+
+    }
+}
